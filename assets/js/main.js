4811b05cde4db3c0aa1afe41cb815050633951cb
Annie Vallone <annievallone@gmail.com>
2018-07-08 19:42:54 -0400

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ var rest = {};
 //calling the API URL with the input value 
 $("#search").on("click", function (event) {
 	event.preventDefault();
-	
 	search = $("#location").val().trim();
 	url = "http://app.ticketmaster.com/discovery/v2/events.json?city=" +
 		search + "&apikey=IVeW1wnw1EgrDASBp2QqlmxszcLjjEKy";
@@ -16,7 +15,6 @@ $("#search").on("click", function (event) {
 		//$("#location").empty();
 	});
 });
-
 async function success(response) {
 	//validate user input
 	if (($.isNumeric(search)) && (search.length >= 5)) {
@@ -28,47 +26,40 @@ async function success(response) {
 		$("#events").empty();
 		$("#location").val("");
 		$("#events").append("<h1>Please enter a valid city</h1>");
-		
 	}
-	
 	else {
 		$("#events").empty();
 		$("#location").val("");
-		//adding the event to the DOM 
-		for (var i = 0; i < response._embedded.events.length; i++) {
-			var eventName = response._embedded.events[i].name;
-			var buyTicket = response._embedded.events[i].url;
-			var eventDate = response._embedded.events[i].dates.start.localDate;
-			var eventPrice = response._embedded.events[i].priceRanges[0].min;
-			var eventImage = response._embedded.events[i].images[0].url;
-			var lng = response._embedded.events[i]._embedded.venues[0].location.longitude;
-			var lat = response._embedded.events[i]._embedded.venues[0].location.latitude;
-			var venue = response._embedded.events[i]._embedded.venues[0].name;
-			var eventID = (eventName + eventDate).replace(/\s+/g,"-").toLowerCase().toString();
-			eventID = eventID.replace(/[^\w\s]/gi, '');
-			// $("#events").append("<div id=" + eventID + ">" + eventName + eventDate + eventPrice + "<img class='size' src=" + eventImage + "></div>");
-			$("#events").append("<div id=" + eventID + ">" +"<br>"+"<br>"+"<img class='size' src=" + eventImage + ">"+"<br>"+ eventName +"<br>"+venue +"<br>"+ eventDate +" "+"Ticket prices: $"+" "+eventPrice +"<br>"+"<a class="+"redLink"+" href=" + buyTicket + ">" +"BuyTicket" +"</a>"+ "</div>");
-			var queryURL = "https://gt-yelp-api.herokuapp.com/api/" + lat + "/" + lng;
-			rest[eventName] = [];
-			var res = await $.get(queryURL);
-		for (var j = 1; j <= 5; j++) {
-			var restaurant = res[j];
-			var resName = res[j].name;
-			var resURL = res[j].url;
-			var number = j;
-			var space = "<p>";
+	//adding the event to the DOM 
+	for (var i = 0; i < response._embedded.events.length; i++) {
+		var eventName = response._embedded.events[i].name;
+		var buyTicket = response._embedded.events[i].url;
+		var eventDate = response._embedded.events[i].dates.start.localDate;
+		var eventPrice = response._embedded.events[i].priceRanges[0].min;
+		var eventImage = response._embedded.events[i].images[0].url;
+		var lng = response._embedded.events[i]._embedded.venues[0].location.longitude;
+		var lat = response._embedded.events[i]._embedded.venues[0].location.latitude;
+		var venue = response._embedded.events[i]._embedded.venues[0].name;
+		var eventID = (eventName + eventDate).replace(/\s+/g,"-").toLowerCase().toString();
+		eventID = eventID.replace(/[^\w\s]/gi, '');
 		
-			var a = "#" + eventID;
-			//a.replace(/\s+/g,"-").toLowerCase();
-			//console.log(a);
-			$(a).append("<a href=" + resURL + ">" + space + number + resName +"</a>");
-			// $(a).append("<a href=" + buyTicket + ">" +"BuyTicket" +"</a>");
-			
-		};
-		
+		$("#events").append("<div class='row mx-auto'><div class='-6'><img class='size' src=" + eventImage + "></div><div class='col-6' id=" + eventID + ">"+ eventName +"<br>"+venue +"<br>"+ eventDate +" "+"Ticket prices: $"+" "+eventPrice +"<br>"+"<a class="+"redLink"+" href=" + buyTicket + ">" +"BuyTicket" +"</a>"+ "</div></div>");
+		var queryURL = "https://gt-yelp-api.herokuapp.com/api/" + lat + "/" + lng;
+		rest[eventName] = [];
+		var res = await $.get(queryURL);
+			for (var j = 1; j <= 5; j++) {
+				var restaurant = res[j];
+				var resName = res[j].name;
+				var resURL = res[j].url;
+				var space = "<p>";
+				var a = "#" + eventID;
+				a.replace(/\s+/g,"-").toLowerCase()  
+				$(a).append("<p><a href=" + resURL + ">" + resName +"</a></p>");     
+			};
 	}
-}
 
-}
+			}
+
+}	
 
 
